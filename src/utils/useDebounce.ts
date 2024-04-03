452afb45f4c6e6ccee289b068a3237c31f3b832a
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-04-03 19:58:48 +0900

    feat:debounce custom hook 추가

---
+++ src/utils/useDebounce.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log(value);
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
