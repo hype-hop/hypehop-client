278d8b248e7dea2c3bf0c3bb587e2d446bc3564b
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-04-08 22:14:00 +0900

    feat:앨범 리뷰 등록 폼 에 선택된 앨범, 앨범 평점 상태 추가
    * RatingAlbum 컴포넌트에 props 로 전달

--- src/components/AlbumReviewForm.tsx
+++ src/components/AlbumReviewForm.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import AlbumSearch from './AlbumSearch';
 import { AlbumSearchResult } from '../types/albumSearch';
-// import RatingAlbum from './RatingAlbum';
+import RatingAlbum from './RatingAlbum';
 
 function AlbumReviewForm() {
   const [searchResult, setSearchResult] = useState<AlbumSearchResult[] | null>(null);
-  const [, setSelectedAlbum] = useState<AlbumSearchResult | null>(null);
+  const [selectedAlbum, setSelectedAlbum] = useState<AlbumSearchResult | null>(null);
+  const [albumRating, setAlbumRating] = useState<number>(0.5);
   return (
     <>
       <AlbumSearch searchResult={searchResult} setSearchResult={setSearchResult} setSelectedAlbum={setSelectedAlbum} />
-      {/* {selectedAlbum && <RatingAlbum album={selectedAlbum!} />} */}
+      {selectedAlbum && <RatingAlbum album={selectedAlbum!} rating={albumRating} setRating={setAlbumRating} />}
     </>
   );
 }
