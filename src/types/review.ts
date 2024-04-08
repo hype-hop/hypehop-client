4d45a63977bd30042fcd05e63cd114f4ee0205ed
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-04-08 15:56:45 +0900

    feat:ReviewsRank 타입 추가
    * 인기 리뷰 타입

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,22 +1,24 @@
-import { User } from "./user";
+import { User } from './user';
 
-
-export interface Review {
-    _id: string;
-    title: string;
-    body: string;
-    thumbnail: string;
-    createdAt: string; 
-    user: User;
-    isFavorite: Array<string>; 
-    comments: Array<Comment>; 
-    albumRating:number
+export interface Comment {
+  _id: string;
+  text: string;
+  createdAt: string;
+  user: User;
 }
 
-  export interface Comment {
-    _id: string;
-    text: string;
-    createdAt: string; 
-    user: User;
-  }
-  
+export interface Review {
+  _id: string;
+  title: string;
+  body: string;
+  thumbnail: string;
+  createdAt: string;
+  user: User;
+  isFavorite: Array<string>;
+  comments: Array<Comment>;
+  albumRating: number;
+}
+
+export interface ReviewsRank extends Review {
+  favoriteCount: number;
+}
