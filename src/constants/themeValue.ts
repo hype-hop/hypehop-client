ade725c2fb9258d0f96eeca9d14492598dc9f6fa
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-04-18 09:49:48 +0900

    feat:theme 관련 값 상수화
    * typography 속성 관련 값 추가
      - 기존 theme 의 variant 로 정의되있던 size, weight, lineHeight, letterSpacing 을 상수로 관리

---
+++ src/constants/themeValue.ts
@@ -0,0 +1,21 @@
+export const typography = {
+  size: {
+    sm: 11,
+    md: 12,
+    lg: 14,
+    xl: 30,
+  },
+  weight: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+  lineHeight: {
+    sm: '-1px',
+    md: '1.24',
+  },
+  letterSpacing: '-4%',
+} as const;
+
+export const palette = {};
