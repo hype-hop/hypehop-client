4fd8935f563e3f3caf0671c2efce6bc09fabe02d
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-04-28 17:34:50 +0900

    feat:내가 쓴 리뷰 컴포넌트
    * MyReview
       - props : MyReview 타입
       - 추후 AlbumReviewSummary 에서 재사용가능하도록 수정 하면 좋을 듯

--- src/pages/MyInformationPage.tsx
+++ src/pages/MyInformationPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, Container, Typography } from '@mui/material';
 import { useAuth } from '../AuthenticationContext';
 import { MyInformation } from '../types/myInformation';
 import getMyInformation from '../api/myInformation';
-import MyReviews from '../components/review/MyReviews';
+import MyReview from '../components/review/MyReview';
 
 function MyPage() {
   const [data, setData] = useState<MyInformation | null>(null);
@@ -12,15 +12,13 @@ function MyPage() {
   const [user] = useAuth();
 
   useEffect(() => {
-    const fetchData = async () => {
+    (async () => {
       const res = await getMyInformation();
 
       if (res.success) {
         setData(res.data);
       }
-    };
-
-    fetchData();
+    })();
   }, []);
 
   if (!user) {
@@ -28,15 +26,25 @@ function MyPage() {
   }
 
   return (
-    <Box>
-      <Box sx={{ display: 'flex', columnGap: '24px' }}>
+    <Container>
+      <Box sx={{ display: 'flex', columnGap: '24px', mt: '40px', mb: '40px' }}>
         <Avatar src={user?.image} sx={{ width: '100px', height: '100px' }} />
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center' }}>
           <Typography variant="h1"> {user?.name} </Typography>
           <Typography>닉네임 변경</Typography>
         </Box>
       </Box>
-    </Box>
+      <Box>
+        <Typography align="left" fontSize="24px" fontWeight="bold" lineHeight="1" mb="16px">
+          내가 작성한 리뷰
+        </Typography>
+        {data?.reviews && (
+          <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '24px' }}>
+            {data?.reviews.map((review) => <MyReview review={review} />)}
+          </Box>
+        )}
+      </Box>
+    </Container>
   );
 }
 
