3cb9ce190a9551f6eef69f724ca0257c9cb3843f
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-04-28 22:13:41 +0900

    feat:MyReview cursor:pointer 추가

--- src/components/review/MyReview.tsx
+++ src/components/review/MyReview.tsx
@@ -24,115 +24,112 @@ function MyReviews({ review }: { review: MyReview }) {
         <Box>
           <AlbumCover albumId={albumId} url={thumbnail} />
         </Box>
-        <Box>
-          <Card
-            key={_id}
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              width: '100%',
-              minHeight: '181px',
 
-              backgroundColor: 'transparent',
-              border: 'none',
-              borderRadius: 'none',
-              boxShadow: 'none',
-            }}
-          >
-            <Box>
-              <Typography fontWeight="bold" fontSize={typography.size.lg} textAlign="left">
-                {albumName}
+        <Card
+          key={_id}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            minHeight: '181px',
+            cursor: 'pointer',
+            backgroundColor: 'transparent',
+            border: 'none',
+            borderRadius: 'none',
+            boxShadow: 'none',
+          }}
+        >
+          <Box>
+            <Typography fontWeight="bold" fontSize={typography.size.lg} textAlign="left">
+              {albumName}
+            </Typography>
+            {artists?.map((artist) => (
+              <Typography color="rgb(182,182,182)" textAlign="left">
+                {artist}
               </Typography>
-              {artists?.map((artist) => (
-                <Typography color="rgb(182,182,182)" textAlign="left">
-                  {artist}
-                </Typography>
-              ))}
+            ))}
+          </Box>
+          <CardContent sx={{ width: '100%', padding: 0 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                whiteSpace: 'nowrap',
+                columnGap: '7px',
+              }}
+            >
+              <Box textAlign="left">
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                    marginLeft: '3px',
+                    columnGap: '8px',
+                  }}
+                />
+                <Rating readOnly value={albumRating} />
+              </Box>
             </Box>
-            <CardContent sx={{ width: '100%', padding: 0 }}>
-              <Box
-                sx={{
-                  display: 'flex',
-                  whiteSpace: 'nowrap',
-                  columnGap: '7px',
-                }}
-              >
-                <Box textAlign="left">
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-start',
-                      marginLeft: '3px',
-                      columnGap: '8px',
-                    }}
-                  />
-                  <Rating readOnly value={albumRating} />
-                </Box>
-              </Box>
 
-              <Box onClick={() => router(`/album/review/${_id}`)}>
-                <Typography
-                  color="white.main"
-                  fontWeight="bold"
-                  fontSize="16px"
-                  component="div"
-                  mt="12px"
-                  sx={{
-                    textAlign: 'left',
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                  }}
-                >
-                  {title}
-                </Typography>
-                <Typography
-                  color="grey.light"
-                  fontSize={typography.size.md}
-                  fontWeight="regular"
-                  component="div"
-                  mt="8px"
-                  sx={{
-                    display: '-webkit-box',
-                    textAlign: 'left',
-                    minHeight: '45px',
-                    lineHeight: '15px',
-                    overflow: 'hidden',
-                    WebkitLineClamp: 3,
-                    WebkitBoxOrient: 'vertical',
-                    textOverflow: 'ellipsis',
-                  }}
-                >
-                  {body.replace(/<[^>]+>/g, ' ')}
-                </Typography>
-              </Box>
-            </CardContent>
-            <CardActions disableSpacing sx={{ width: '100%', mt: '13px', padding: 0 }}>
-              <Box
+            <Box onClick={() => router(`/album/review/${_id}`)}>
+              <Typography
+                color="white.main"
+                fontWeight="bold"
+                fontSize="16px"
+                component="div"
+                mt="12px"
                 sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'flex-start',
-                  columnGap: '8px',
+                  textAlign: 'left',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
                 }}
               >
-                <Favorite reviewId={_id} numberOfFavorite={isFavorite.length} />
-                <Link
-                  to={`/album/review/${_id}`}
-                  style={{ display: 'inline-flex', textDecoration: 'none', color: 'inherit' }}
-                >
-                  <Box sx={{ display: 'flex', alignItems: 'center', columnGap: '1px' }}>
-                    <CommentIcon />
-                    <Typography sx={{ color: 'rgb(168,168,168)', fontSize: '12px' }}>
-                      댓글 {comments.length}개
-                    </Typography>
-                  </Box>
-                </Link>
-              </Box>
-            </CardActions>
-          </Card>
-        </Box>
+                {title}
+              </Typography>
+              <Typography
+                color="grey.light"
+                fontSize={typography.size.md}
+                fontWeight="regular"
+                component="div"
+                mt="8px"
+                sx={{
+                  display: '-webkit-box',
+                  textAlign: 'left',
+                  minHeight: '45px',
+                  lineHeight: '15px',
+                  overflow: 'hidden',
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: 'vertical',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                {body.replace(/<[^>]+>/g, ' ')}
+              </Typography>
+            </Box>
+          </CardContent>
+          <CardActions disableSpacing sx={{ width: '100%', mt: '13px', padding: 0 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+                columnGap: '8px',
+              }}
+            >
+              <Favorite reviewId={_id} numberOfFavorite={isFavorite.length} />
+              <Link
+                to={`/album/review/${_id}`}
+                style={{ display: 'inline-flex', textDecoration: 'none', color: 'inherit' }}
+              >
+                <Box sx={{ display: 'flex', alignItems: 'center', columnGap: '1px' }}>
+                  <CommentIcon />
+                  <Typography sx={{ color: 'rgb(168,168,168)', fontSize: '12px' }}>댓글 {comments.length}개</Typography>
+                </Box>
+              </Link>
+            </Box>
+          </CardActions>
+        </Card>
       </Box>
     </Box>
   );
