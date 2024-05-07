d430dc9a9f7e5a258887537371dcce2e16eebd52
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-05-07 15:38:16 +0900

    최근 리뷰 디자인 수정 (#78)
    * feat:Review interface 수정
    
    * albumId, albumTitle, artists 추가
    
    * feat:최근 리뷰 디자인 수정
    
    * AlbumReviewSummary 추가

--- src/types/review.d.ts
+++ src/types/review.d.ts
@@ -17,7 +17,10 @@ export interface Review {
   user: User;
   isFavorite: Array<FavoriteClickedUser>;
   comments: Array<Comment>;
+  albumId: string;
+  albumTitle: string;
   albumRating: number;
+  artists: string[];
 }
 
 export interface MyReview extends Review {
