fc1e299b517e4a85530873c06b3bc22c69ea5610
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-05-10 13:00:06 +0900

    feat:앨범 상세 페이지 앨범 정보 컴포넌트 props 타입 수정
    * AlbumDetailType -> AlbumData

--- src/components/album/AlbumDetail/AlbumDetailInformation.tsx
+++ src/components/album/AlbumDetail/AlbumDetailInformation.tsx
@@ -1,8 +1,8 @@
 import { Box, Rating, Typography } from '@mui/material';
-import { AlbumDetailType } from '../../../types/albumDetail';
 import { typography } from '../../../constants/themeValue';
+import { AlbumData } from '../../../types/albumData';
 
-function AlbumDetailInformation({ data }: { data: AlbumDetailType }) {
+function AlbumDetailInformation({ data }: { data: AlbumData }) {
   return (
     <Box
       sx={{
@@ -27,7 +27,7 @@ function AlbumDetailInformation({ data }: { data: AlbumDetailType }) {
           <Typography fontWeight="bold">{data?.albumData.name}</Typography>
           <Box sx={{ display: 'flex' }}>
             {data?.albumData.artists.map((artist, index) => (
-              <Typography color="grey.main">
+              <Typography key={index} color="grey.main">
                 {artist.name}{' '}
                 {data?.albumData.artists.length > 1 && index < data!.albumData!.artists!.length - 1 && '· '}
               </Typography>
