e21c02c59fcf881df59afb2bd4dbe76a86f57c73
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-05-22 16:08:27 +0900

    pull develop

--- src/components/album/AlbumDetail/AlbumDetailInformation.tsx
+++ src/components/album/AlbumDetail/AlbumDetailInformation.tsx
@@ -1,9 +1,10 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Rating, Skeleton, Typography } from '@mui/material';
 import { typography } from '../../../constants/themeValue';
 import { AlbumData } from '../../../types/albumData';
 import CustomStar from '../../review/CustomStar';
+import PRECISION from '../../../constants/ratingPrecision';
 
-function AlbumDetailInformation({ data }: { data: AlbumData }) {
+function AlbumDetailInformation({ data }: { data?: AlbumData }) {
   return (
     <Box
       sx={{
@@ -17,15 +18,24 @@ function AlbumDetailInformation({ data }: { data: AlbumData }) {
       }}
     >
       <Box sx={{ display: 'flex' }}>
-        <Box
-          component="img"
-          width="60px"
-          height="60px"
-          src={data?.albumData.images[1].url}
-          sx={{ borderRadius: '6.6px', marginRight: '20px' }}
-        />
+        {data?.albumData.images ? (
+          <Box
+            component="img"
+            width="60px"
+            height="60px"
+            src={data?.albumData.images[1].url}
+            sx={{ borderRadius: '6.6px', marginRight: '20px' }}
+          />
+        ) : (
+          <Skeleton variant="rounded" width="60px" height="60px" sx={{ marginRight: '20px' }} />
+        )}
+
         <Box sx={{ textAlign: 'left' }}>
-          <Typography fontWeight="bold">{data?.albumData.name}</Typography>
+          {data?.albumData.name ? (
+            <Typography fontWeight="bold">{data?.albumData.name}</Typography>
+          ) : (
+            <Skeleton variant="rounded" />
+          )}
           <Box sx={{ display: 'flex' }}>
             {data?.albumData.artists.map((artist, index) => (
               <Typography key={index} color="grey.main">
@@ -42,6 +52,31 @@ function AlbumDetailInformation({ data }: { data: AlbumData }) {
               </Typography>
             </Box>
           )}
+
+          <Box sx={{ display: 'flex', mt: '2px' }}>
+            {data?.albumData.artists.length > 0 ? (
+              data?.albumData.artists.map((artist, index) => (
+                <Typography key={index} color="grey.main">
+                  {artist.name}{' '}
+                  {data?.albumData.artists.length > 1 && index < data!.albumData!.artists!.length - 1 && '· '}
+                </Typography>
+              ))
+            ) : (
+              <Skeleton variant="rounded" width="40px" />
+            )}
+          </Box>
+          {!data?.albumRatingAverage && <Skeleton />}
+          {data &&
+            (Number.isNaN(data?.albumRatingAverage) ? (
+              <Typography>평점이 없습니다.</Typography>
+            ) : (
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Rating value={Number(data?.albumRatingAverage)} readOnly precision={PRECISION} />
+                <Typography component="div" fontSize={typography.size.md} fontWeight="medium" lineHeight={1}>
+                  {data?.albumRatingAverage}
+                </Typography>
+              </Box>
+            ))}
         </Box>
       </Box>
     </Box>
