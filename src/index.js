4ce9588fa36f8a5f244b101656c6597d6d272c7b
chyon8 <48877469+chyon8@users.noreply.github.com>
2024-05-29 12:39:00 +0900

    react-snap test (#115)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,53 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-import-module-exports */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import './index.css';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './AuthenticationContext';
 import theme from './theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </ThemeProvider>
-    </AuthProvider>
-  </React.StrictMode>,
-);
+// Ensure this is the correct root element ID from your HTML
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+const renderApp = (Component) => {
+  if (rootElement.hasChildNodes()) {
+    hydrateRoot(
+      rootElement,
+      <React.StrictMode>
+        <AuthProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <HelmetProvider>
+              <Component />
+            </HelmetProvider>
+          </ThemeProvider>
+        </AuthProvider>
+      </React.StrictMode>,
+    );
+  } else {
+    createRoot(rootElement).render(
+      <React.StrictMode>
+        <AuthProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <HelmetProvider>
+              <Component />
+            </HelmetProvider>
+          </ThemeProvider>
+        </AuthProvider>
+      </React.StrictMode>,
+    );
+  }
+};
+
+renderApp(App);
+
 reportWebVitals();
