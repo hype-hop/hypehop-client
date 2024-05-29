0fe5a27cae169bf237b8ac6a6b8de11df8b54287
chyon8 <chyon8@hotmail.com>
2024-05-29 12:47:29 +0900

    delete react-snap

--- src/index.js
+++ src/index.js
@@ -1,53 +1,25 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-import-module-exports */
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './AuthenticationContext';
 import theme from './theme';
 
-// Ensure this is the correct root element ID from your HTML
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
-
-const renderApp = (Component) => {
-  if (rootElement.hasChildNodes()) {
-    hydrateRoot(
-      rootElement,
-      <React.StrictMode>
-        <AuthProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <HelmetProvider>
-              <Component />
-            </HelmetProvider>
-          </ThemeProvider>
-        </AuthProvider>
-      </React.StrictMode>,
-    );
-  } else {
-    createRoot(rootElement).render(
-      <React.StrictMode>
-        <AuthProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <HelmetProvider>
-              <Component />
-            </HelmetProvider>
-          </ThemeProvider>
-        </AuthProvider>
-      </React.StrictMode>,
-    );
-  }
-};
-
-renderApp(App);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </ThemeProvider>
+    </AuthProvider>
+  </React.StrictMode>,
+);
 reportWebVitals();
