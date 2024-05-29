073f1c9e6ce66b8e420e6819303da4ed07c270bc
chyon8 <chyon8@hotmail.com>
2024-05-29 15:19:44 +0900

    fix metatag: dynamic desc

--- src/components/common/Header/MetaTag.tsx
+++ src/components/common/Header/MetaTag.tsx
@@ -12,10 +12,15 @@ export default function MetaTag(props: MetaTagProps) {
   return (
     <Helmet>
       <title>{(props.title && `${props.title}`) || '하입합 | 앨범 리뷰'}</title>
-      <meta name="description" content={props.description || ''} />
+      <meta
+        name="description"
+        content={
+          props.description || '하입합, hypehop, 앨범 리뷰, 힙합, 음악, 차트, 앨범 차트, 힙합 차트, 앨범, 앨범 평점'
+        }
+      />
       <meta property="og:title" content={props.title || ' 리뷰 평점'} />
       <meta property="og:site_name" content="하입합" />
-      <meta property="og:description" content={props.description || 'default description'} />
+      <meta property="og:description" content={props.description || ''} />
       <meta property="og:image" content={props.imgSrc || 'image'} />
     </Helmet>
   );
