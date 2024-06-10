e9cb992d0c6d6d0a1db52ffcb8671ad9c72ea2e7
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-06-10 22:53:02 +0900

    feat&refactor:유저 이미지, 이름 클릭시 프로필 페이지로 이동 및 ReviewsPage 기존 컴포넌트로 리팩토링
    * AlbumReviewSummary 의 유저 이미지, 이름 클릭 시 프로필 페이지 이동
    * ReviewsPage
       - AlbumCover, AlbumReviewSummary 로 대체

--- src/pages/ReviewsPage.tsx
+++ src/pages/ReviewsPage.tsx
@@ -1,26 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  Box,
-  CardActions,
-  Avatar,
-  CircularProgress,
-} from '@mui/material';
+import { Button, Typography, Box, CircularProgress } from '@mui/material';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import he from 'he';
-import { ReactComponent as CommentIcon } from '../assets/icons/comment.svg';
-import TimeSincePost from '../components/album/TimeSincePost';
-import Favorite from '../components/common/Favorite';
 import BASE_URL from '../config';
 import { Review } from '../types/review';
-import { typography } from '../constants/themeValue';
-import CustomStar from '../components/review/CustomStar';
 import ReviewsPageSkeleton from '../components/common/skeletons/reviewsPage/ReviewsPageSkeleton';
+import AlbumCover from '../components/album/AlbumCover';
+import AlbumReviewSummary from '../components/review/AlbumReviewSummary';
 
 interface InitialData {
   totalPage: number;
@@ -174,139 +159,23 @@ function ReviewsPage() {
       >
         {Array.isArray(data?.reviews) ? (
           data?.reviews.map((review) => (
-            <Card
-              key={review._id}
+            <Box
               sx={{
-                display: 'flex',
-                flexDirection: 'column',
-
-                height: '423px',
                 padding: '16px',
-                bgcolor: 'background.default',
-                border: '1px solid',
-                borderColor: 'rgb(52,52,52)',
+                border: '1px solid rgb(52, 52, 52)',
                 borderRadius: '0px 16px 16px 16px',
-                width: '100%',
-                margin: '0 auto',
-                maxWidth: '100%',
               }}
             >
-              <Link target="_blank" to={`/album/review/${review._id}`}>
-                <CardMedia component="img" width="234px" height="234px" image={review.thumbnail} alt="album cover" />
-              </Link>
-
-              <CardContent sx={{ padding: '0' }}>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    mt: '13px',
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                  }}
-                >
-                  <Box>
-                    <Avatar
-                      style={{ width: '28px', height: '28px', borderRadius: '50%' }}
-                      src={review?.user?.image}
-                      alt="user"
-                    />
-                  </Box>
-
-                  <Box display="grid">
-                    <Box display="flex">
-                      <Typography
-                        variant="body1"
-                        sx={{
-                          alignContent: 'center',
-                          ml: 1,
-                          maxWidth: '100px',
-                          overflow: 'hidden',
-                          textOverflow: 'ellipsis',
-                          whiteSpace: 'nowrap',
-                        }}
-                      >
-                        {/* <Link to={`/user/${review.user._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                          {review.user.name || review.user.displayName}
-                      </Link> */}
-                        <Typography>{review.user.name || review.user.displayName}</Typography>
-                      </Typography>
-
-                      <Typography
-                        sx={{
-                          color: 'rgb(168, 168, 168)',
-                          ml: '4px',
-                          textAlign: 'left',
-                          alignContent: 'center',
-                        }}
-                      >
-                        <TimeSincePost createdAt={review.createdAt} />{' '}
-                      </Typography>
-                    </Box>
-                    <Box sx={{ ml: '4px' }}>
-                      <CustomStar edit={false} value={review.albumRating} />
-                    </Box>
-                  </Box>
-                </Box>
-
-                <Link to={`/album/review/${review._id}`} style={{ color: 'inherit', textDecoration: 'none' }}>
-                  <Box sx={{ width: '234px', height: '74px' }}>
-                    <Typography
-                      color="white.main"
-                      fontWeight={typography.weight.bold}
-                      fontSize={typography.size.lg}
-                      component="div"
-                      sx={{
-                        textAlign: 'left',
-                        mt: '13px',
-                        whiteSpace: 'nowrap',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                      }}
-                    >
-                      {review.title}
-                    </Typography>
-                    <Typography
-                      color="grey.light"
-                      fontSize={typography.size.md}
-                      fontWeight={typography.weight.regular}
-                      component="div"
-                      sx={{
-                        textAlign: 'left',
-                        mt: '6px',
-                        height: '45px',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        lineHeight: '15px',
-                        letterSpacing: '-4%',
-                      }}
-                    >
-                      {he.decode(review.body.replace(/<[^>]+>/g, ' '))}
-                    </Typography>
-                  </Box>
-                </Link>
-              </CardContent>
-              <CardActions disableSpacing sx={{ mt: 'auto' }}>
-                <Box
-                  sx={{
-                    display: 'flex',
-                  }}
-                >
-                  <Favorite reviewId={review?._id} favoriteClickedUsers={review?.isFavorite} />
-                  <Link
-                    to={`/album/review/${review._id}`}
-                    style={{ display: 'inline-flex', textDecoration: 'none', color: 'inherit' }}
-                  >
-                    <Box sx={{ display: 'flex', alignItems: 'center', columnGap: '1px', ml: '9px' }}>
-                      <CommentIcon />
-                      <Typography color="grey.main" fontSize={typography.size.sm}>
-                        댓글 {review.comments.length}개
-                      </Typography>
-                    </Box>
-                  </Link>
-                </Box>
-              </CardActions>
-            </Card>
+              <AlbumCover
+                reviewId={review._id}
+                url={review.thumbnail}
+                albumTitle={review.albumTitle}
+                artists={review.artists}
+              />
+              <Box width="200px" mt={2}>
+                <AlbumReviewSummary review={review} />
+              </Box>
+            </Box>
           ))
         ) : (
           <ReviewsPageSkeleton />
