f847324fdfa47374e9a74592203b4589afb038d9
오현재 <hyunjae1995@gmail.com>
2025-09-29 23:58:30 +0900

    chore: add Googleusercontent to image domains

--- next.config.js
+++ next.config.js
@@ -26,6 +26,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.scdn.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
