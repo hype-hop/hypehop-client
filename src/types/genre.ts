8e657d3307361a7a15632a45a4ec2399e03c2a54
오현재 <hyunjae1995@gmail.com>
2025-11-13 23:41:07 +0900

    feat:add Genre enum type

---
+++ src/types/genre.ts
@@ -0,0 +1,12 @@
+enum Genre {
+  all = '전체',
+  hiphop = '#힙합',
+  rnb = '#알앤비',
+  rock = '#록',
+  electronic = '#일렉트로닉',
+  pop = '#팝',
+  jazz = '#재즈',
+  etc = '#기타',
+}
+
+export default Genre;
