72601e557c0d7337d752a0b409cf7d918f2532b0
오현재 <hyunjae1995@gmail.com>
2025-11-17 23:22:31 +0900

    feat:add ReviewChart and ReviewChartCard components

---
+++ src/components/review/ReviewChartCard.tsx
@@ -0,0 +1,87 @@
+import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { typography } from '../../constants/themeValue';
+import CustomStars from './CustomStar';
+import { ReviewsRank } from '../../types/review';
+
+export default function ReviewChartCard({ review, index }: { review: ReviewsRank; index: number }) {
+  const item = review;
+
+  return (
+    <Card
+      sx={{
+        bgcolor: 'background.default',
+        maxWidth: { xs: '100%', sm: '300px', md: '100%', lg: '100%' },
+        height: '60px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        padding: '0px',
+        flex: 'none',
+        order: '0',
+        flexGrow: '0',
+        margin: '16px 0px',
+        boxShadow: 'none',
+      }}
+    >
+      <CardMedia
+        component="img"
+        width="60px"
+        height="60px"
+        image={item.thumbnail}
+        alt="album cover"
+        sx={{
+          maxWidth: '60px',
+          minWidth: '60px',
+          borderRadius: '6.6px',
+        }}
+      />
+      <CardContent sx={{ marginLeft: '16px' }}>
+        <Typography fontSize={typography.size.lg} fontWeight="medium">
+          {index + 1}
+        </Typography>
+      </CardContent>
+      <CardContent
+        sx={{
+          marginTop: '8px',
+          maxWidth: '63vw',
+          display: 'flex',
+          flexDirection: 'column',
+          paddingTop: '0px',
+          rowGap: '2px',
+        }}
+      >
+        <Typography
+          align="left"
+          fontSize={typography.size.md}
+          fontWeight={typography.weight.regular}
+          sx={{ margin: '4px 0px', color: 'rgb(168, 168, 168)' }}
+          component="div"
+        >
+          {item.artists || item.albumTitle.split('-', 2)[0]}
+        </Typography>
+        <Typography
+          align="left"
+          fontSize={typography.size.lg}
+          fontWeight={typography.weight.bold}
+          sx={{
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            wordBreak: 'break-all',
+          }}
+          component="div"
+        >
+          {item.artists ? item.albumTitle : item.albumTitle.split('-', 2)[1]}
+        </Typography>
+
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <CustomStars name="half-rating-read" value={item.albumRating} readOnly />
+          <Typography sx={{ ml: '4px', mt: '4px' }} fontSize="fontSizeXs">
+            {item.albumRating.toFixed(1)}
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
