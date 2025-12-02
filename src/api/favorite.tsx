1d7406b29ca8c88e3f2203b22df0bb20343ce3a9
오현재 <hyunjae1995@gmail.com>
2025-12-02 16:40:58 +0900

    feat: update fetchReviewFavoriteUsers to include credentials

--- src/api/favorite.tsx
+++ src/api/favorite.tsx
@@ -1,11 +1,12 @@
+import { headers } from 'next/headers';
 import BASE_URL from '../config';
 import { FavoriteClickedUser } from '../types/favorite';
 
 const fetchReviewFavoriteUsers = async (reviewId: string): Promise<FavoriteClickedUser[] | null> => {
   try {
     const url = `${BASE_URL}/api/review/${reviewId}/favorites`;
-    const result = await (await fetch(url)).json();
-    return result.data;
+    const result = await fetch(url, { credentials: 'include' });
+    return await result.json();
   } catch (error) {
     return null;
   }
