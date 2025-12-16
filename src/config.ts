3ac69dc8cb8856d36fc2fbf9ebfbd62cf1cf782e
오현재 <hyunjae1995@gmail.com>
2025-12-16 15:43:06 +0900

    chore: Set BASE_URL based on NODE_ENV in config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL =
+  process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_API_URL : 'https://server.hypehop.co.kr';
 
 export default BASE_URL;
