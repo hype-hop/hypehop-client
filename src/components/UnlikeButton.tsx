df81952af9ba1736f07e972f9ebc254a0c063405
오현재 <hyunjae1995@gmail.com>
2025-12-31 00:09:18 +0900

    ui: hide UnlikeButton on small screens

--- src/components/UnlikeButton.tsx
+++ src/components/UnlikeButton.tsx
@@ -62,6 +62,7 @@ function UnlikeButton({ parentRef }: { parentRef: RefObject<HTMLDivElement | nul
         },
         translate: `${position.x}px ${position.y}px`,
         transition: 'translate 1s',
+        display: { xs: 'none', md: 'block' },
       }}
     >
       싫어요 ㅜㅜ
