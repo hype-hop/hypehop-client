5948fb28dedd7bdee47c15cb3b622d6610a62d16
오현재 <hyunjae1995@gmail.com>
2026-01-27 00:27:00 +0900

    ui: simplify Footer component and adjust spacing

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,6 @@
-import { Box, Typography, IconButton } from '@mui/material';
+import { Box } from '@mui/material';
 import Link from 'next/link';
 import LogoIcon from '../../assets/icons/logo-main.svg';
-import InstagramIcon from '../../assets/icons/instagram.svg';
 
 export default function Footer() {
   return (
@@ -11,27 +10,14 @@ export default function Footer() {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        mt: '50px',
-        mb: '50px',
+        mt: '100px',
+        mb: '75px',
         position: 'sticky',
         top: '100vh',
       }}
     >
       <LogoIcon width={143} height={20} />
-      <Link href="mailto:hypehopcom@gmail.com" style={{ textDecoration: 'none' }}>
-        <Typography
-          sx={{
-            color: 'rgb(174, 174, 174)',
-            mt: '10px',
-            mb: '16px',
-          }}
-        >
-          hypehopcom@gmail.com
-        </Typography>
-      </Link>
-      <IconButton href="https://www.instagram.com/hype__hop/">
-        <InstagramIcon />
-      </IconButton>
+      <Link href="mailto:hypehopcom@gmail.com" style={{ textDecoration: 'none' }} />
     </Box>
   );
 }
