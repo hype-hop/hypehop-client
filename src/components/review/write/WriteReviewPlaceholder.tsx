45421bd2c2a2dbd4cc3a5298b4b811d318418e85
오현재 <hyunjae1995@gmail.com>
2026-01-29 20:15:28 +0900

    refactor: WriteReviewPlaceholder layout and style

--- src/components/review/write/WriteReviewPlaceholder.tsx
+++ src/components/review/write/WriteReviewPlaceholder.tsx
@@ -3,7 +3,6 @@ import { Box, Typography } from '@mui/material';
 const commonBoxStyles = {
   border: '1px solid',
   backgroundColor: 'rgb(52, 52, 52)',
-  opacity: '10%',
   borderRadius: '16px',
   borderColor: 'rgb(52, 52, 52)',
   pl: '16px',
@@ -12,59 +11,44 @@ const commonBoxStyles = {
   mt: '10px',
 };
 
-const commonTypoStyles = {
-  mt: '40px',
-  opacity: '10%',
-};
 function WriteReviewPlaceholder() {
   return (
-    <>
-      <Typography
-        variant="h1"
-        sx={{
-          ...commonTypoStyles,
-        }}
-      >
-        앨범 평점
-      </Typography>
-      <Box sx={{ ...commonBoxStyles }}>
-        <Typography textAlign="left">앨범을 추가해 확인하세요.</Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        cursor: 'not-allowed',
+        rowGap: '24px',
+        WebkitMaskImage: 'linear-gradient(to bottom, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0) 100%)',
+        maskImage: 'linear-gradient(to bottom, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0) 100%)',
+      }}
+    >
+      <Box>
+        <Typography variant="h1">앨범 평점</Typography>
+        <Box sx={{ ...commonBoxStyles }}>
+          <Typography textAlign="left">앨범을 추가해 확인하세요.</Typography>
+        </Box>
       </Box>
-      <Typography
-        sx={{
-          ...commonTypoStyles,
-        }}
-        variant="h1"
-      >
-        트랙별 평점
-      </Typography>
-      <Box sx={{ ...commonBoxStyles }}>
-        <Typography textAlign="left">트랙리스트를 열어 확인하세요.</Typography>
-      </Box>
-      <Typography
-        sx={{
-          ...commonTypoStyles,
-        }}
-        variant="h1"
-      >
-        공개여부
-      </Typography>
-      <Box sx={{ ...commonBoxStyles }}>
-        <Typography textAlign="left">공개</Typography>
-      </Box>
+      <Box>
+        <Typography variant="h1">트랙별 평점</Typography>
 
-      <Typography
-        sx={{
-          ...commonTypoStyles,
-        }}
-        variant="h1"
-      >
-        리뷰작성하기
-      </Typography>
-      <Box sx={{ ...commonBoxStyles }}>
-        <Typography textAlign="left">제목을 입력하세요.</Typography>
+        <Box sx={{ ...commonBoxStyles }}>
+          <Typography textAlign="left">앨범을 추가해 확인하세요.</Typography>
+        </Box>
       </Box>
-    </>
+      <Box>
+        <Typography variant="h1">공개여부</Typography>
+        <Box sx={{ ...commonBoxStyles }}>
+          <Typography textAlign="left">앨범을 추가해 확인하세요.</Typography>
+        </Box>
+      </Box>
+      <Box>
+        <Typography variant="h1">리뷰작성하기</Typography>
+        <Box sx={{ ...commonBoxStyles }}>
+          <Typography textAlign="left">앨범을 추가해 확인하세요.</Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
